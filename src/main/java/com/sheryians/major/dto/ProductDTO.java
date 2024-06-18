39da6cedc684493ba9a206f7eb49c7368acdd240
paycp <rrikhsibaev@gmail.com>
2024-06-18 14:46:45 +0200

    ThirdOne

---
+++ src/main/java/com/sheryians/major/dto/ProductDTO.java
@@ -0,0 +1,19 @@
+package com.sheryians.major.dto;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private int categoryId;
+    private double price;
+    private double weight;
+    private String description;
+    private String imageName;
+
+}
